7ee4b17c7af468b0271e4fc5cb8f690c53f8e451
MrunalThakre <101035846+MrunalThakre@users.noreply.github.com>
2023-07-30 12:02:55 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,51 @@
+document.querySelectorAll('.image-container img').forEach(image =>{
+    image.onclick = () =>{
+    document.querySelector('.popup-image').style.display='block';
+    document.querySelector('.popup-image img').src=image.getAttribute('src');
+    }
+});
+
+document.querySelector('.popup-image span').onclick=() =>{
+    document.querySelector('.popup-image').style.display='none';
+
+}
+
+
+let slideIndex = 0;
+let timeoutId = null;
+const slides = document.getElementsByClassName("mySlides");
+const dots = document.getElementsByClassName("dot");
+
+showSlides();
+function currentSlide(index) {
+     slideIndex = index;
+     showSlides();
+}
+function plusSlides(step) {
+  
+  if(step < 0) {
+      slideIndex -= 2;
+      
+      if(slideIndex < 0) {
+        slideIndex = slides.length - 1;
+      }
+  }
+  
+  showSlides();
+}
+function showSlides() {
+  for(let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    dots[i].classList.remove('active');
+  }
+  slideIndex++;
+  if(slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].classList.add('active');
+   if(timeoutId) {
+      clearTimeout(timeoutId);
+   }
+  timeoutId = setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
